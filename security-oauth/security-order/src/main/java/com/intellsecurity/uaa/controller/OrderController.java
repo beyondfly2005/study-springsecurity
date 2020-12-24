d7d1f7e5a8d838b60fba614db6c46ea3dd2929d5
beyondSoft <498601485@qq.com>
2020-12-24 22:39:24 +0800

    取消注释

--- security-oauth/security-order/src/main/java/com/intellsecurity/uaa/controller/OrderController.java
+++ security-oauth/security-order/src/main/java/com/intellsecurity/uaa/controller/OrderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderController {
     @RequestMapping("r1")
-//    @PreAuthorize("hasAnyAuthority('p1')")
+    @PreAuthorize("hasAnyAuthority('p1')")
     public String r1(){
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
 //        String name
