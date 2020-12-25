469c98609eb207f96972f5ed0310825741fa751a
gaolongfei <498601485@qq.com>
2020-12-25 17:28:01 +0800

    modify

---
+++ security-oauth/security-res1/src/main/java/com/intellsecurity/uaa/config/ResrourceServerConfig.java
@@ -0,0 +1,57 @@
+package com.intellsecurity.uaa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+/**
+ * Copyright: Copyright (c) 2020 Anchuang Network Technology Co., Ltd
+ *
+ * @ClassName: ResrourceServerConfig
+ * @Description: 该类的描述
+ * @version: V0.1.0
+ * @author: gaolongfei
+ * @date: 2020/12/24 11:38
+ */
+@Configuration
+@EnableResourceServer
+public class ResrourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    //资源ID
+    private static final String RESOURCE_ID ="res1";
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(RESOURCE_ID)//资源 id
+                .tokenStore(tokenStore)//验证令牌
+                // .tokenServices(tokenService())//验证令牌的服务
+                .stateless(true);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").access("#oauth2.hasScope('ROLE_ADMIN')")
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//基于token 不记录session
+    }
+    //资源服务令牌解析服务,每次进行远程校验会影响性能，所以应该采用jwt方式来进行校验
+//    @Bean
+//    public ResourceServerTokenServices tokenService() {
+//        //使用远程服务请求授权服务器校验token,必须指定校验token 的url、client_id，client_secret
+//        RemoteTokenServices service=new RemoteTokenServices();
+//        service.setCheckTokenEndpointUrl("http://localhost:52020/uaa/oauth/check_token");
+//        service.setClientId("c1");
+//        service.setClientSecret("secert");
+//        return service;
+//    }
+
+}
